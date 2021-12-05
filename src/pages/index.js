bab99b5878ce65f328fd5bcad843f8833d903ef2
Brandon Fillpot <brandonfillpot@gmail.com>
2021-12-04 17:23:28 -0800

    styled home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,19 @@
+import { Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
+import * as styles from "../styles/home.module.css"
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={styles.header}>
         <div>
           <h2>Design</h2>
-          <h2>Build</h2>
-          <h2>Deploy</h2>
+          <h3>Develop & Deploy</h3>
+          <p>Full stack developer based in San Jose, CA</p>
+          <Link className={styles.btn} to="/projects">
+            See My Portfolio
+          </Link>
         </div>
       </section>
     </Layout>
